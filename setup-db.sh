20da508960bba9165e8160875c18e3b32af99832
Julien Barreau <julien.barreau@forestadmin.com>
2023-06-30 11:47:33 +0200

    chore: improve setup-db.sh

--- setup-db.sh
+++ setup-db.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+if [ -f .env ] ; then
+    forest_server_url=$(cat .env | grep "FOREST_SERVER_URL")
+    forest_auth_secret=$(cat .env | grep "FOREST_AUTH_SECRET")
+    forest_env_secret=$(cat .env | grep "FOREST_ENV_SECRET")
+else
+    forest_server_url=$(cat .env-template | grep "FOREST_SERVER_URL")
+    forest_auth_secret=$(cat .env-template | grep "FOREST_AUTH_SECRET")
+    forest_env_secret=$(cat .env-template | grep "FOREST_ENV_SECRET")
+fi
+
 echo "# Database varialbes" > .env
 echo "DATABASE_URL=$1://example:password@127.0.0.1:5443/example" >> .env
 echo "DOCKER_DATABASE_URL=$1://example:password@host.docker.internal:5443/example" >> .env
 
+echo "" >> .env
 echo "# Docker variables" >> .env
 cat .env-template | grep "APPLICATION_PORT" >> .env
 echo "DEPENDS_DB=$1" >> .env
+
+echo "" >> .env
+echo "# Forest variables" >> .env
+echo "$forest_server_url" >> .env
+echo "$forest_auth_secret" >> .env
+echo "$forest_env_secret" >> .env
+

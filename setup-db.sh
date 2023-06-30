cf325dd0149c1f762922e5750d91d784108efd72
Julien Barreau <julien.barreau@forestadmin.com>
2023-06-30 11:32:57 +0200

    chore(bash): setup.sh & docker improvement

--- setup-db.sh
+++ setup-db.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+echo "# Database varialbes" > .env
 echo "DATABASE_URL=$1://example:password@127.0.0.1:5443/example" >> .env
 echo "DOCKER_DATABASE_URL=$1://example:password@host.docker.internal:5443/example" >> .env
+
+echo "# Docker variables" >> .env
+cat .env-template | grep "APPLICATION_PORT" >> .env
+echo "DEPENDS_DB=$1" >> .env
